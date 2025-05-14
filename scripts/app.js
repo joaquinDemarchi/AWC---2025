cb0afa805c9494c144891ee3d74e436765672321
JoaquinDemarchi <joa.demarchi@gmail.com>
2025-05-14 12:36:30 -0300

    agregar paginas y conexiones faltantes del proyecto

--- scripts/app.js
+++ scripts/app.js
@@ -50,31 +50,37 @@ const contenedorProductos = document.querySelector('.product-container');
 const contenedorOfertas = document.querySelector('.ofertas-container');
 
 function createProductCard(product) {
+    
     //crea tarjeta produto
     const card = document.createElement('article');
     card.classList.add('producto');
 
-    //crea imagen
+    //va creando los elementos de la tarjeta
+
+    //imagen
     const img = document.createElement('img');
     img.src = product.image;
     img.alt = product.name;
 
-    //crea titulo
+    //titulo
     const title = document.createElement('h3');
     title.textContent = product.name;
 
-    //crea descrip
+    //descrip
     const description = document.createElement('p');
     description.textContent = product.description;
 
-    //crea precio
+    //precio
     const price = document.createElement('p');
     price.textContent = `$${product.price}`;
     price.classList.add('precioProd');
 
-    //crea boton
+
+    //boton compra
     const button = document.createElement('button');
     button.textContent = 'Comprar';
+    button.setAttribute("onclick","window.location.href = './detalleProducto.html'")
+    
 
     //crea añade todos los elementos a la tarjeta
     card.appendChild(img);
@@ -83,29 +89,14 @@ function createProductCard(product) {
     card.appendChild(price);
     card.appendChild(button);
 
+    
     return card;
 }
 
-// function addProduct() {
-//     const newProduct = {
-//         name: "Nuevo Producto",
-//         description: "Descripción del nuevo producto",
-//         image: "./img/image-google.png",
-//         price: 20
-//     };
-
-//     const card = createProductCard(newProduct);
-//     contenedorProductos.appendChild(card);
-// }
 
 products.forEach( product => {
     const card = createProductCard(product);
     contenedorProductos.appendChild(card);
 });
 
-
-
-// const button = document.querySelector('#btn-add-products');
-
-// button.addEventListener('click', addProduct);
 
